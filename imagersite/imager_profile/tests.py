27ec78d5732c9eb3201451bd17dd7307061544f8
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-23 17:22:34 -0700

    filled in commented out test values

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -3,11 +3,12 @@ from .models import ImagerProfile, User
 import factory
 import random
 
+
 class UserFactory(factory.django.DjangoModelFactory):
     """ defines a mock user instance for testing """
     class Meta:
         model = User
-    
+
     username = factory.Faker('user_name')
     email = factory.Faker('email')
 
@@ -16,16 +17,16 @@ class ProfileFactory(factory.django.DjangoModelFactory):
     """ defines a mock profile instance for testing """
     class Meta:
         model = ImagerProfile
-    
-    # bio = factory.Faker('')
+
+    bio = 'Tandon Holderbone was a pioneer of sorts only indulging in the finest things life has to offer...'
     phone = factory.Faker('phone_number')
-    # location = factory.Faker('')
+    location = factory.Faker('state_abbr')
     website = factory.Faker('domain_name')
     fee = round(random.uniform(0, 100), 2)
     is_active = factory.Faker('boolean')
-    # camera = factory.Faker('')
-    # services = factory.Faker('')
-    # photostyles = factory.Faker('')
+    camera = 'DSLR'
+    services = 'landscape'
+    photostyles = 'night'
 
 
 class ProfileUnitTests(TestCase):
@@ -35,19 +36,19 @@ class ProfileUnitTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super(TestCase, cls)
-        for _ in range (50):
+        for _ in range(50):
             user = UserFactory.create()
             user.set_password(factory.Faker('password'))
             user.save()
 
             profile = ProfileFactory.create(user=user)
             profile.save()
-    
+
     @classmethod
     def tearDownClass(cls):
         super(TestCase, cls)
         User.objects.all().delete()
-    
+
     def test_user_can_see_its_profle(self):
         one_user = User.objects.first()
         self.assertIsNotNone(one_user.profile)
