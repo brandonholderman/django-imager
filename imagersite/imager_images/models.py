723adaf74f8402ff289287af037ea2947d378a98
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-25 13:48:07 -0700

    created photo and album models

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from sorl.thumbnail import ImageField
 
+
 # Create your models here.
 class Album(models.Model):
     """Class starting properties of photo album"""
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
-    cover_image = models.ForeignKey('Photo', on_delete=models.CASCADE, related_name='+', null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='albums')
+    cover_image = models.ForeignKey('Photo', on_delete=models.CASCADE,
+                                    related_name='+', null=True, blank=True)
     name = models.CharField(max_length=180, default='Untitled')
     description = models.TextField(blank=True, null=True)
     date_created = models.DateField(auto_now_add=True)
@@ -23,3 +26,26 @@ class Album(models.Model):
     def __str__(self):
         """ string method for class """
         return '{}'.format(self.name)
+
+
+class Photo(models.Model):
+    """Class starting properties of each photo"""
+    album = models.ForeignKey(Album, on_delete=models.CASCADE,
+                              related_name='photos')
+    image = ImageField(upload_to='images')
+    title = models.CharField(max_length=180, default='Untitled')
+    description = models.TextField(blank=True, null=True)
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField(blank=True, null=True)
+    published = models.CharField(
+        max_length=7,
+        choices=(('PRIVATE', 'Private'),
+                 ('SHARED', 'Shared'),
+                 ('PUBLIC', 'Public')
+                 )
+    )
+
+    def __str__(self):
+        """ string method for class """
+        return '{}'.format(self.title)
