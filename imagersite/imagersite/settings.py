a9f70a33bdf73161e2a33daf5f6f53e0a2cf013c
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-25 13:53:38 -0700

    made migrations and added pillow to requirements

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
     'imagersite',
     'imager_profile',
+    'imager_images',
 ]
 
 MIDDLEWARE = [
