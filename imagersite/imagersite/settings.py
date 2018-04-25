dd010e60ffb0ae5577f1326b23f92a5cd858384e
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-25 13:56:56 -0700

    added media url and root

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -133,6 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
 
 # Django Registration Settings
 ACCOUNT_ACTIVATION_DAYS = 1
