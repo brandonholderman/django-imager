cfb653851177e14d9ab1e41b527d6a1685548e26
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-26 16:14:58 -0700

    added profile and method to display random image on home page

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from imager_images.models import Photo
+from random import choice
 
 
 def home_view(request):
-    return render(request, 'generic/home.html', {'message': 'Hellew World'})
+    photos = Photo.objects.filter(published='PUBLIC').all()
+    # import pdb; pdb.set_trace()
+    rand_pick = choice(photos)
+
+    context = {
+        'photos': rand_pick
+    }
+
+    return render(request, 'generic/home.html', context)
