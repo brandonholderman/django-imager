15e8058dfa27a343e45f7da3b3ca6a13f7a33fe3
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-26 20:36:56 -0700

    building library view, error when attempting to load page

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -33,3 +33,27 @@ def settings_view(request, username=None):
     context = {}
 
     return render(request, 'imager_profile/profile.html', context)
+
+
+def library_view(request, username=None):
+    owner = False
+
+    if not username:
+        username = request.user.get_username()
+        owner = True
+        if username == '':
+            return redirect('home')
+
+    albums = Album.objects.filter(user__username=username)
+    photos = Photo.objects.filter(album__user__username=username)
+
+    if not owner:
+        photos = Photo.objects.filter(published='PUBLIC').all()
+        albums = Album.objects.filter(published='PUBLIC').all()
+
+        context = {
+            'photos': photos,
+            'albums': albums,
+        }
+
+    return render(request, 'imager_profile/library.html', context)
