d2cc083eab16076479514cf51918f5e94358c7bd
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-05-17 11:56:37 -0700

    installs django cors package and put necessary changes for it in settings.py

--- imagersite/rest_api/urls.py
+++ imagersite/rest_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PhotoListApi
+from .views import PhotoListApi, UserApi
 from rest_framework.authtoken import views
 
 
 urlpatterns = [
     path('photo/', PhotoListApi.as_view(), name='photo_list_api'),
+    path('user/', UserApi.as_view(), name='user-detail'),
     path('auth-token-login/', views.obtain_auth_token),
 ]
--- imagersite/rest_api/views.py
+++ imagersite/rest_api/views.py
@@ -1,11 +1,35 @@
-from rest_framework import generics
-from .serlializers import PhotoSerializer
+from rest_framework import generics, status
+from .serlializers import PhotoSerializer, UserSerializer
 from imager_images.models import Photo
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
 
 
 class PhotoListApi(generics.ListAPIView):
     """Docstring."""
+    permission_classes = (IsAuthenticated,)
     serializer_class = PhotoSerializer
 
     def get_queryset(self):
         return Photo.objects.filter(album__user=self.request.user)
+
+
+class UserApi(generics.RetrieveAPIView, generics.CreateAPIView):
+    """Docstring."""
+    permission_classes = ''
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = UserSerializer
+
+    def retrieve(self, request, pk=None):
+        if not pk:
+            return Response(UserSerializer(
+                request.user).data, status=status.HTTP_200_OK)
+        return super().retrieve(request, pk)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
